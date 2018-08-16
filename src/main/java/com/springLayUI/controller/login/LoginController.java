5cf213804b39106927f3601cbdf2c6c30362f97f
luyang <luy5817@163.com>
2018-08-16 11:35:50 +0800

    配置文件更新

--- src/main/java/com/springLayUI/controller/login/LoginController.java
+++ src/main/java/com/springLayUI/controller/login/LoginController.java
@@ -12,9 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/main")
 public class LoginController {
-    @RequestMapping("login")
-    public String login(){
-        System.out.println("login in...");
-       return "/main/login";
-    }
+    /**
+     * 主页
+     * @return
+     */
+    @RequestMapping("/index")
+    public String main(){return "/main/index";}
+
+    /**
+     * 登陆页
+     * @return
+     */
+    @RequestMapping("/login")
+    public String login(){return "/main/login";}
+
 }
